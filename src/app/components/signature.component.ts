6fa6069ee2ada85965a473ff84bf01f58d82f7fd
Nick Polizogopoulos <133910144+nickpolizogopoulos@users.noreply.github.com>
2024-03-26 21:26:26 +0200

    Added linktree, font colouring and adjustable space between the icons

--- src/app/components/signature.component.ts
+++ src/app/components/signature.component.ts
@@ -8,6 +8,7 @@ import { Component, ElementRef, ViewChild } from "@angular/core";
             <div class="row">
                 <div class="col">
 
+                    <!-- FIRST SECTION -->
                     <section class="boxSection mt-4">
                         <h1 class="mb-3 h2 mainblue">Signature Generator Application</h1>
                         <p class="lead">Welcome to the Signature Generator Application! Make your email signature fast and easy!</p>
@@ -19,6 +20,7 @@ import { Component, ElementRef, ViewChild } from "@angular/core";
                         </ol>
                     </section>
 
+                    <!-- INFORMATION INPUTS -->
                     <section class="boxSection">
                         <h4 class="mb-3">Enter your information</h4>
                         <div class="row">
@@ -36,6 +38,7 @@ import { Component, ElementRef, ViewChild } from "@angular/core";
                         </div>
                     </section>
 
+                    <!-- SOCIAL CHECKBOXES -->
                     <section class="boxSection">
                         <h4 class="mb-3">Which social media platforms would you like to include in your signature?</h4>
                         <div class="row mb-2">
@@ -89,6 +92,12 @@ import { Component, ElementRef, ViewChild } from "@angular/core";
                             </div>
                             <div class="col-lg-2 col-xxl-1">
                                 <div class="form-check">
+                                    <input  [(ngModel)]="linktree" class="form-check-input" type="checkbox" id="linktree">
+                                    <label class="noselect" for="linktree">Linktree</label>
+                                </div>
+                            </div>
+                            <div class="col-lg-2 col-xxl-1">
+                                <div class="form-check">
                                     <label (click)="checkAll()" class="noselect"><u>Check all</u></label>
                                 </div>
                             </div>
@@ -100,7 +109,8 @@ import { Component, ElementRef, ViewChild } from "@angular/core";
                         </div>
                     </section>
 
-                    <section *ngIf="website || facebook || instagram || linkedin || github || twitter || reddit || threads" class="boxSection">
+                    <!-- SOCIAL LINKS -->
+                    <section *ngIf="website || facebook || instagram || linkedin || github || twitter || reddit || threads || linktree" class="boxSection">
                         <h4>Paste your social media links below</h4>
                         <small>[ In case you write any links by hand, <u>make sure you include "https://" infront of them</u> - e.g. <strong>https://</strong>google.com or <strong>https://</strong>www.google.com ]</small>
                         <div class="row mt-3">
@@ -113,13 +123,15 @@ import { Component, ElementRef, ViewChild } from "@angular/core";
                                 <input *ngIf="github" [(ngModel)]="githubLink" type="text" class="form-control" placeholder="GitHub">
                                 <input *ngIf="reddit" [(ngModel)]="redditLink" type="text" class="form-control" placeholder="Reddit">
                                 <input *ngIf="threads" [(ngModel)]="threadsLink" type="text" class="form-control" placeholder="Threads">
+                                <input *ngIf="linktree" [(ngModel)]="linktreeLink" type="text" class="form-control" placeholder="Linktree">
                             </div>
                         </div>
                     </section>
 
+                    <!-- SIGNATURE PREFERENCES -->
                     <section class="boxSection">
                         <h4 class="mb-3">Signature Preferences</h4>
-                        <div class="row mb-2">
+                        <div class="row mb-5">
                             <div class="col-lg-2 col-xxl-2">
                                 <div class="form-check">
                                     <input [(ngModel)]="boldName" class="form-check-input" type="checkbox" id="boldName">
@@ -151,8 +163,43 @@ import { Component, ElementRef, ViewChild } from "@angular/core";
                                 </div>
                             </div>
                         </div>
+                        <h4 class="mt-3 mb-3">Space between the social media icons</h4>
+                        <div class="col-md-8 col-xl-7 col-xxl-5">
+                            <input [(ngModel)]="socialIconSpace" type="range" class="form-range" min="3" max="15">
+                        </div>
+                        <p class="lead">{{socialIconSpace}} pixels</p>
                     </section>
 
+                    <!-- COLOURS -->
+                    <section class="boxSection">
+                        <div class="row">
+
+                            <div class="col-xl-6 mb-3">
+                                <div class="col-xl-8">
+                                    <h4 class="mb-3">Name, Surname, Tel. and Email colour</h4>
+                                    <label class="mb-2" for="pickerA">Use the color picker</label>
+                                    <div><input class="mb-2" [(ngModel)]="textAColour" type="color" id="pickerA"></div>
+                                    <p>{{textAColour}}</p>
+                                    <label class="mb-2" for="colorA">Specify your color <span class="form-text ms-3">[ make sure you include the hashtag # symbol - e.g. #1e2f5a ]</span></label>
+                                    <input class="form-control" id="colorA" [(ngModel)]="textAColour" type="text" placeholder="#000000">
+                                </div>
+                            </div>
+
+                            <div class="col-xl-6 mb-3">
+                                <div class="col-xl-8">
+                                    <h4 class="mb-3">Title, telephone number and email address colour</h4>
+                                    <label class="mb-2" for="pickerB">Use the color picker</label>
+                                    <div><input class="mb-2" [(ngModel)]="textBColour" type="color" id="pickerB"></div>
+                                    <p>{{textBColour}}</p>
+                                    <label class="mb-2" for="colorB">Specify your color <span class="form-text ms-3">[ make sure you include the hashtag # symbol - e.g. #1e2f5a ]</span></label>
+                                    <input class="form-control" id="colorB" [(ngModel)]="textBColour" type="text"placeholder="#000000">
+                                </div>
+                            </div>
+
+                        </div>
+                    </section>
+
+                    <!-- SIGNATURE -->
                     <section class="boxSection">
                         <h4 class="mb-4">Your Signature</h4>
                         <table #signature cellspacing="0" cellpadding="4" style="background-color: white; font-family: 'Montserrat', sans-serif; line-height: 1.3;" class="signature">
@@ -164,7 +211,7 @@ import { Component, ElementRef, ViewChild } from "@angular/core";
                                         <table cellspacing="0" cellpadding="2">
                                             <tbody>
                                                 <tr>
-                                                    <td style="font-size: 17px;">
+                                                    <td style="font-size: 17px; color: {{textAColour}};">
                                                         <span *ngIf="!boldName && !underlineName">{{name}} {{surname}}</span>
                                                         <strong *ngIf="boldName && !underlineName">{{name}} {{surname}}</strong>
                                                         <u *ngIf="!boldName && underlineName">{{name}} {{surname}}</u>
@@ -172,7 +219,7 @@ import { Component, ElementRef, ViewChild } from "@angular/core";
                                                     </td>
                                                 </tr>
                                                 <tr>
-                                                    <td style="font-size: 12px;">
+                                                    <td style="font-size: 12px; color: {{textBColour}};">
                                                         <span *ngIf="!boldTitle && !underlineTitle">{{title}}</span>
                                                         <strong *ngIf="boldTitle && !underlineTitle">{{title}}</strong>
                                                         <u *ngIf="!boldTitle && underlineTitle">{{title}}</u>
@@ -184,28 +231,30 @@ import { Component, ElementRef, ViewChild } from "@angular/core";
                                                     <td style="font-size: 12px;">
                                                         <table cellspacing="0" cellpadding="2" style="font-size: 12px;">
                                                             <tbody>
-                                                                <tr><td><span><strong>Tel.: </strong></span><span style="text-decoration:none; color: black;">{{phone}}</span></td></tr>
-                                                                <tr><td><span><strong>Email: </strong></span><a style="text-decoration:none; color: black;" href = "mailto:{{email}}">{{email}}</a></td></tr>
-                                                                <tr><td>{{location}}</td></tr>
+                                                                <tr><td><span style="color: {{textAColour}};"><strong>Tel.: </strong></span><span style="text-decoration:none; color: {{textBColour}};">{{phone}}</span></td></tr>
+                                                                <tr><td><span style="color: {{textAColour}};"><strong>Email: </strong></span><a style="text-decoration:none; color: {{textBColour}};" href = "mailto:{{email}}">{{email}}</a></td></tr>
+                                                                <tr><td style="color: {{textBColour}};">{{location}}</td></tr>
                                                                 <tr>
                                                                     <table>
                                                                         <tbody>
                                                                             <tr>
-                                                                                <td *ngIf="websiteLink"><a href="{{websiteLink}}"><img width="21" height="auto" src="../assets/social/web-logo.png" alt="website icon"></a></td>
-                                                                                <td *ngIf="website" width="7"></td> 
+                                                                                <td *ngIf="website"><a href="{{websiteLink}}"><img width="21" height="auto" src="../assets/social/web-logo.png" alt="website icon"></a></td>
+                                                                                <td *ngIf="website" width="{{socialIconSpace}}"></td>
                                                                                 <td *ngIf="facebook"><a href="{{facebookLink}}"><img width="21" height="auto" src="../assets/social/facebook-logo.png" alt="linkedin icon"></a></td>
-                                                                                <td *ngIf="facebook" width="7"></td>
+                                                                                <td *ngIf="facebook" width="{{socialIconSpace}}"></td>
                                                                                 <td *ngIf="instagram"><a href="{{instagramLink}}"><img width="21" height="auto" src="../assets/social/instagram-logo.png" alt="instagram icon"></a></td>
-                                                                                <td *ngIf="instagram" width="7"></td>
+                                                                                <td *ngIf="instagram" width="{{socialIconSpace}}"></td>
                                                                                 <td *ngIf="linkedin"><a href="{{linkedinLink}}"><img width="21" height="auto" src="../assets/social/linkedin-logo.png" alt="linkedin icon"></a></td>
-                                                                                <td *ngIf="linkedin" width="7"></td>
+                                                                                <td *ngIf="linkedin" width="{{socialIconSpace}}"></td>
                                                                                 <td *ngIf="github"><a href="{{githubLink}}"><img width="21" height="auto" src="../assets/social/github-logo.png" alt="github icon"></a></td>
-                                                                                <td *ngIf="github" width="7"></td>
+                                                                                <td *ngIf="github" width="{{socialIconSpace}}"></td>
                                                                                 <td *ngIf="twitter"><a href="{{twitterLink}}"><img width="21" height="auto" src="../assets/social/x-logo.png" alt="x icon"></a></td>
-                                                                                <td *ngIf="twitter" width="7"></td>
+                                                                                <td *ngIf="twitter" width="{{socialIconSpace}}"></td>
                                                                                 <td *ngIf="reddit"><a href="{{redditLink}}"><img width="21" height="auto" src="../assets/social/reddit-logo.png" alt="reddit icon"></a></td>
-                                                                                <td *ngIf="reddit" width="7"></td>
+                                                                                <td *ngIf="reddit" width="{{socialIconSpace}}"></td>
                                                                                 <td *ngIf="threads"><a href="{{threadsLink}}"><img width="21" height="auto" src="../assets/social/threads-logo.png" alt="reddit icon"></a></td>
+                                                                                <td *ngIf="threads" width="{{socialIconSpace}}"></td>
+                                                                                <td *ngIf="threads"><a href="{{linktreeLink}}"><img width="21" height="auto" src="../assets/social/linktree-logo.png" alt="reddit icon"></a></td>
                                                                             </tr>
                                                                         </tbody>
                                                                     </table>
@@ -252,6 +301,7 @@ export class SignatureComponent {
     twitter:boolean = false;
     reddit:boolean = false;
     threads:boolean = false;
+    linktree:boolean = false;
 
     websiteLink:string | undefined = '';
     facebookLink:string | undefined = '';
@@ -261,6 +311,7 @@ export class SignatureComponent {
     twitterLink:string | undefined = '';
     redditLink:string | undefined = '';
     threadsLink:string | undefined = '';
+    linktreeLink:string | undefined = '';
     @ViewChild('signature', { static: false }) signature!:ElementRef;
 
     boldName:boolean = false;
@@ -268,6 +319,9 @@ export class SignatureComponent {
     underlineName:boolean = false;
     underlineTitle:boolean = false;
     lineAfterName:boolean = false;
+    textAColour:string = '#000000';
+    textBColour:string = '#000000';
+    socialIconSpace:string = '7';
 
     spinnerLoading:boolean = false;
     copyResponse:boolean = false;
@@ -309,7 +363,9 @@ export class SignatureComponent {
         this.twitter = false;
         this.reddit = false;
         this.threads = false;
+        this.linktree = false;
     }
+
     checkAll():void {
         this.website = true;
         this.facebook = true;
@@ -319,6 +375,7 @@ export class SignatureComponent {
         this.twitter = true;
         this.reddit = true;
         this.threads = true;
+        this.linktree = true;
     }
 
     copySignature():void {
